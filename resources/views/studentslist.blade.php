1cd2aca140fcc11b3d5e840b376515059f8c2674
MaryamMahmoud <dreamer.me62@gmail.com>
2021-12-16 15:12:04 +0200

    First Commit

---
+++ resources/views/studentslist.blade.php
@@ -0,0 +1,40 @@
+<div class="card mb-3">
+    <img src="https://cdn.pixabay.com/photo/2017/01/25/05/10/read-2007118__340.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">List of students</h5>
+        <p class="card-text">You can find here all the informatins about the students in the system</p>
+
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">First name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Speciality</th>
+                <th scope="col">Operations</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->cne }}</td>
+                    <td>{{ $student->firstName }}</td>
+                    <td>{{ $student->lastName }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->speciality }}</td>
+                    <td>
+
+                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-info">Edit</a>
+                        <a href="{{ url('/destroy/'.$student->id) }}" class="btn btn-sm btn-danger">Delete</a>
+
+                    </td>
+
+
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
